00412364557a20e3e34c073a2f1f4238db6ff05c
Alan Alves <alan.oliveira@teledatabrasil.com.br>
2025-06-08 09:11:41 -0300

    Player Components, inputs

---
+++ src/components/Player.jsx
@@ -0,0 +1,40 @@
+import { useState } from "react";
+
+export default function Player({ inicialName, symbol }) {
+
+    const [playerName, SetPlayerName] = useState(inicialName);
+    const [isEditing, setIsEditing] = useState(false)
+
+    function handleClick() {
+        setIsEditing(isEditing => !isEditing);
+    }
+    
+    function handleChange(event){        
+        SetPlayerName(event.target.value)
+    }
+
+    let editablePlayerName = (
+        <span onClick={handleClick} className="player-name">{playerName}</span>
+    );
+
+    if (isEditing) {
+        editablePlayerName = (            
+            //two way binding => pegar o valor do onchange e colocar de volta no input
+            
+            <input type="text" required  value={playerName} onChange={handleChange} ></input>
+
+            // <input type="text" required  defaultValue={name}></input> // uma forma de editar mas sem travar o valor
+        )
+    }
+
+    return (
+        <li>
+            <span className="player">
+                {editablePlayerName}
+                <span className="player-symbol">{symbol}</span>
+            </span>
+            <button onClick={handleClick}>{isEditing ? 'Save' : 'Edit'}</button>
+        </li>
+
+    );
+}
